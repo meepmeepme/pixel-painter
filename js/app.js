a650a8b6350313dee09a902f42e72cf66bc6d2b2
meepmeepme <jrdevleague15@Juniors-iMac-6.local>
2016-12-17 12:00:49 -1000

    pixels were made

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+function changeColorOnClick(idName) {
+	console.log('enteringthedangerzone');
+	var originalElementColor = document.getElementById('selectedColorName');
+	document.getElementById(idName).style.backgroundColor = originalElementColor;
+
+}
+
+function getBackgroundColor(idName) {
+	var element = document.getElementById(idName);
+	var style = window.getComputedStyle(element,null).getPropertyValue("background-color");
+	console.log(style);
+	document.getElementById("selectedColorName").style.backgroundColor = style;
+
+}
